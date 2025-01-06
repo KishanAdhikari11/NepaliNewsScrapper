f479d232daae95829db1b99ef1857b824fd781b4
Kishan <adhikarykishan11@gmail.com>
2025-01-06 14:20:36 +0545

    directly run on main.py

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 from onlinekhabar import OnlinekhabarScraper
+from nayapatrika import NayaPatrikaScraper
+from ratopati import RatopatiScraper
+from gorkhapatra import GorkhapatraScraper
+from setopati import SetopatiScraper
+from ekantipur import EkantipurkhabarScraper
+from base import BaseScraper
+
+SCRAPERS = {
+    "onlinekhabar": OnlinekhabarScraper,
+    "nayapatrika": NayaPatrikaScraper,
+    "ratopati": RatopatiScraper,
+    "gorkhapatra": GorkhapatraScraper,
+    "setopati": SetopatiScraper,
+    "ekantipur":EkantipurkhabarScraper
+}
 
 def main():
-    url = "https://www.onlinekhabar.com/2024/03/1443168"
-    scraper = OnlinekhabarScraper(url)
-    news_json = scraper.get_news_json()
+    url = input("Enter Url: ")
+    base_scraper = BaseScraper(url)  
+    portal_name = base_scraper.portal_name  
+    print("Detected portal:", portal_name)
+    scrapper_class=SCRAPERS.get(portal_name)
+    scrapper=scrapper_class(url)
+    news_json=scrapper.get_news()
     print(news_json)
+
 
 if __name__ == "__main__":
     main()
